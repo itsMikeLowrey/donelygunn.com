9cc663e91dcb0896c679797f9ba401fe8fc0cb5d
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-24 13:35:14 -0800

    Fixed background video

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,12 @@
   <v-container  text-xs-center fluid ma-0 pa-0 >
       <v-flex xs12 ma-0 pa-0 hidden-sm-and-down>
         <video style="
-            position:absolute;
-            height:100%;
-            width:100%;
-            overflow: hidden;"
+        position: fixed;
+        right: 0;
+        bottom: 0;
+        min-width: 100%;
+        min-height: 100%;
+        overflow: hidden;"
         src="https://res.cloudinary.com/dylevfpbl/video/upload/v1551009310/decenMain/yes.mp4" autoplay muted loop/>
       </v-flex>
 
