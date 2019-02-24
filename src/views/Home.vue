949bd20ae1f5245a1d22f0645f4606c1e51f1edc
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-24 03:23:06 -0800

    New img and public folder

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
-  <HelloWorld />
+  <v-container  text-xs-center fluid ma-0 pa-0 >
+      <v-flex xs12 ma-0 pa-0 hidden-sm-and-down style="
+    position:absolute;
+    height:100%;
+    width:100%;
+    overflow: hidden;">
+        <video src="https://res.cloudinary.com/dylevfpbl/video/upload/v1551007271/decenMain/Team-Work.mp4" autoplay muted loop/>
+      </v-flex>
+
+  </v-container>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld'
+// @ is an alias to /src
 
 export default {
+  name: 'home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
+<style scoped>
+
+</style>
