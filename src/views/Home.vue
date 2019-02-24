e2a1aeebc3c6e1f9c326b9332dc5e9448eb8b804
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-24 13:29:48 -0800

    Fullscreen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,14 @@
 <template>
   <v-container  text-xs-center fluid ma-0 pa-0 >
-      <v-flex xs12 ma-0 pa-0 hidden-sm-and-down style="
-    position:absolute;
-    height:100%;
-    width:100%;
-    overflow: hidden;">
-        <video src="https://res.cloudinary.com/dylevfpbl/video/upload/v1551009310/decenMain/yes.mp4" autoplay muted loop/>
+      <v-flex xs12 ma-0 pa-0 hidden-sm-and-down>
+        <video style="
+            position:absolute;
+            height:100%;
+            width:100%;
+            overflow: hidden;"
+        src="https://res.cloudinary.com/dylevfpbl/video/upload/v1551009310/decenMain/yes.mp4" autoplay muted loop/>
       </v-flex>
-      
+
 
   </v-container>
 </template>
