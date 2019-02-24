93e434ca945cf00b56368d6969ceab8aba578992
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-24 13:21:39 -0800

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
     height:100%;
     width:100%;
     overflow: hidden;">
-        <video src="https://res.cloudinary.com/dylevfpbl/video/upload/v1551009310/decenMain/yes.mp4" autoplay muted loop/>
+        
       </v-flex>
       <div/>
 
